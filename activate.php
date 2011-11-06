e70e1836f18287584d9fe3030fd74ccf1d0395ee
Cash Costello <cash.costello@gmail.com>
2011-11-06 16:48:43 -0500

    need to update/add subtype based on whether it exists

--- activate.php
+++ activate.php
@@ -3,4 +3,8 @@
  * Register the ElggAdminNote class for the object/admin_note subtype
  */
 
-add_subtype('object', 'admin_note', 'ElggAdminNote');
+if (get_subtype_id('object', 'admin_note')) {
+	update_subtype('object', 'admin_note', 'ElggAdminNote');
+} else {
+	add_subtype('object', 'admin_note', 'ElggAdminNote');
+}
